0787375709dacf6509590ab7a94b11c9a48673e7
shubhamshii <87321774+shubhamshii@users.noreply.github.com>
2021-07-13 11:40:07 +0530

    Add files via upload

---
+++ star_2/src/co/scp/Star_2.java
@@ -0,0 +1,13 @@
+package co.scp;
+
+public class Star_2 {
+public static void main(String[] args) {
+	for(int i =1;i<=4;i++) {
+		for(int j=1;j<=i;j--) {
+			System.out.print(" * ");
+		}
+		System.out.println();
+	}
+	
+}
+}
